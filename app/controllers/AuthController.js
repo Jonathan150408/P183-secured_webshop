17605e8b48a137f0124dc82553e4c4d336c610d6
neo-darbellay <196329763+Jonathan150408@users.noreply.github.com>
2026-03-25 12:09:24 +0100

    feat(hash des pwd) : commencer à hash les pwd et remplacer les anciens
    [WIP][70]

--- app/controllers/AuthController.js
+++ app/controllers/AuthController.js
@@ -1,4 +1,5 @@
 const db = require("../config/db");
+const argon2 = require("argon2");
 
 module.exports = {
   // ----------------------------------------------------------
@@ -32,16 +33,19 @@ module.exports = {
   // ----------------------------------------------------------
   // POST /api/auth/register
   // ----------------------------------------------------------
-  register: (req, res) => {
+  register: async (req, res) => {
     const { username, email, password, address, photoPath } = req.body;
+    //hash le pwd
+    const hashPassword = await argon2.hash(password).p;
+    console.log("pwd hash " + hashPassword + " pwd " + password);
 
     //on s'en fiche que photoPath soit vide pour le moment
     if (!username || !email || !password || !address) {
       return res.status(400).json({ error: "Un des champs requis est vide" });
     }
 
-    //id et role sont fixes -> 3 et user
-    const query = `INSERT INTO users (id, username, email, password, role, address, photo_path) VALUES (3, '${username}', '${email}', '${password}', 'user', '${address}', '${photoPath}');`;
+    //id et role sont fixes -> 4 et user
+    const query = `INSERT INTO users (id, username, email, password, role, address, photo_path) VALUES (4, '${username}', '${email}', '${hashPassword}', 'user', '${address}', '${photoPath}');`;
 
     db.query(query, (err, results) => {
       if (err) {
