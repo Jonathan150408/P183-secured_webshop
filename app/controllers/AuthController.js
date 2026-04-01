f35cddc336389bef9b3c8c3a723efa16f8811181
jonathan150408 <196329763+Jonathan150408@users.noreply.github.com>
2026-04-01 10:29:33 +0200

    feat, doc(5 - poivre) : ajouter le poivre
    [75][Done]
    Cela à été long, car j'ai eu des soucis avec argon2.verify, le requête et réponse SQL et la redirection utilisateur (que j'ai fini par abandonner car pas obligatoire)

--- app/controllers/AuthController.js
+++ app/controllers/AuthController.js
@@ -12,21 +12,35 @@ module.exports = {
       return res.status(400).json({ error: "Email et mot de passe requis" });
     }
 
-    const query = `SELECT * FROM users WHERE email = '${email}' AND password = '${password}'`;
+    //ajouter le poivre au pwd
+    const pepper = process.env.PEPPER;
+    let passwordWithPepper = password + pepper;
 
-    db.query(query, (err, results) => {
+    const query = `SELECT password FROM users WHERE email = '${email}'`;
+
+    db.query(query, async (err, results) => {
+      //si erreur
       if (err) {
         return res.status(500).json({ error: err.message, query: query });
       }
-
+      //si rien n'est retourné (email incorrect ou pwd inexistant)
       if (results.length === 0) {
         return res
-          .status(401)
+          .status(400)
           .json({ error: "Email ou mot de passe incorrect" });
       }
-
-      res.json({ message: "Connexion réussie", user: results[0] });
-      window.location.href = "/"; // ramène sur accueil
+      //sinon on vérifie le hash
+      const accessGranted = await argon2.verify(
+        results[0].password,
+        passwordWithPepper,
+      );
+      if (accessGranted) {
+        //mot infos utilisateur ok -> connexion
+        res.status(200).json({ message: "Connexion réussie" });
+      } else {
+        //mdp faux -> message erreur plus générique
+        res.status(400).json({ message: "Email ou mot de passe incorrect" });
+      }
     });
   },
 
@@ -36,8 +50,11 @@ module.exports = {
   register: async (req, res) => {
     const { username, email, password, address, photoPath } = req.body;
 
+    //ajouter le poivre au pwd
+    const pepper = process.env.PEPPER;
+    let passwordWithPepper = password + pepper;
     //hash le pwd
-    const hashPassword = await argon2.hash(password);
+    const hashPassword = await argon2.hash(passwordWithPepper);
 
     //on s'en fiche que photoPath soit vide pour le moment
     if (!username || !email || !password || !address) {
@@ -45,14 +62,13 @@ module.exports = {
     }
 
     //id et role sont fixes -> 4 et user
-    const query = `INSERT INTO users (id, username, email, password, role, address, photo_path) VALUES (4, '${username}', '${email}', '${hashPassword}', 'user', '${address}', '${photoPath}');`;
+    const query = `INSERT INTO users (id, username, email, password, role, address, photo_path) VALUES (2, '${username}', '${email}', '${hashPassword}', 'user', '${address}', '${photoPath}');`;
 
     db.query(query, (err, results) => {
       if (err) {
         return res.status(500).json({ error: err.message, query: query });
       } else {
         res.status(201).json({ message: "Création réussie", user: results[0] });
-        // window.location.href = "/"; // ramène sur accueil
       }
     });
   },
--- doc/R183-rapport.md
+++ doc/R183-rapport.md
@@ -34,3 +34,60 @@
 > ```js
 > const hashPassword = await argon2.hash(password);
 > ```
+
+### 5 Ajouter un poivre
+
+> Nous allons maintenant ajouter le poivre à notre application, dans le fichier _.env_ à la racine du projet, ajoutez `PEPPER=9f3c2a8e7b1d4c6f8a91e2b5c7d9f0a1bd55672d7ecdef0ad6c46739ebcaef0`. Sentez-vous libre de changer la valeur du poivre.  
+> Ensuite nous ajoutons le poivre au mot de passe ainsi (toujours la même partie du code):
+>
+> ```js
+> //ajouter le poivre au pwd
+> const pepper = process.env.PEPPER;
+> let passwordWithPepper = password + pepper;
+> //hash le pwd
+> const hashPassword = await argon2.hash(passwordWithPepper);
+> ```
+>
+> On en profite pour update le login (plus haut dans le même fichier), à présent nous commençons par récupérer le poivre et l'ajoutons au password.  
+> On va aussi update un peu la requête SQL afin de ne recevoir que les infos utiles et enfin on vérifie le hash. Voici à quoi devrait ressembler la méthode login.
+>
+> ```js
+> login: (req, res) => {
+>    const { email, password } = req.body;
+>
+>    if (!email || !password) {
+>      return res.status(400).json({ error: "Email et mot de passe requis" });
+>    }
+>
+>    //ajouter le poivre au pwd
+>    const pepper = process.env.PEPPER;
+>   let passwordWithPepper = password + pepper;
+>
+>    const query = `SELECT password FROM users WHERE email = '${email}'`;
+>
+>    db.query(query, async (err, results) => {
+>      //si erreur
+>      if (err) {
+>        return res.status(500).json({ error: err.message, query: query });
+>      }
+>      //si rien n'est retourné (email incorrect ou pwd inexistant)
+>      if (results.length === 0) {
+>        return res
+>          .status(400)
+>          .json({ error: "Email ou mot de passe incorrect" });
+>      }
+>      //sinon on vérifie le hash
+>      const accessGranted = await argon2.verify(
+>        results[0].password,
+>        passwordWithPepper,
+>      );
+>     if (accessGranted) {
+>        //mot infos utilisateur ok -> connexion
+>        res.status(200).json({ message: "Connexion réussie" });
+>      } else {
+>        //mdp faux -> message erreur plus générique
+>       res.status(400).json({ message: "Email ou mot de passe incorrect" });
+>     }
+>  });
+> },
+> ```
